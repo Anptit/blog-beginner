9e2d155cb317f7fe6e6f293015c2adc8bd3b1797
anph <anph@bap.jp>
2024-06-25 10:56:17 +0700

    commit 1

---
+++ app/Contracts/Repositories/GreetingRepositoryInterface.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Contracts\Repositories;
+
+use App\Contracts\Repositories\RepositoryInterface;
+use Illuminate\Database\Eloquent\Builder as EBuilder;
+use Illuminate\Database\Query\Builder;
+
+interface GreetingRepositoryInterface extends RepositoryInterface
+{
+    public function getLastPost(Builder|EBuilder $query);
+
+    public function getPost1Week(Builder|EBuilder $query);
+
+    public function getPost1Month(Builder|EBuilder $query);
+
+    public function getPost6Months(Builder|EBuilder $query);
+
+    public function getPost1Year(Builder|EBuilder $query);
+
+    public function getPostMore1Year(Builder|EBuilder $query);
+
+    public function greeting();
+
+    public function comment();
+}
