80b1807b294908652e120311898fd071f81fba9e
anph <anph@bap.jp>
2024-06-25 17:18:01 +0700

    commit for main branch

--- app/Repositories/Eloquents/PostRepository.php
+++ app/Repositories/Eloquents/PostRepository.php
@@ -81,4 +81,9 @@ class PostRepository extends BaseRepository implements PostRepositoryInterface
     {
         return true;
     }
+
+    public function makeFriends()
+    {
+        return "Friend";
+    }
 }
